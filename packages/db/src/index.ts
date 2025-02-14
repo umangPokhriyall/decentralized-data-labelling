1f3e059fc95f5968bcdbdfe94184b59eb322fa49
Umang Pokhriyal <umangpokhriyall@gmail.com>
2025-02-14 17:57:41 +0530

    updated .env

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,12 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prismaClient = new PrismaClient().$transaction(
-    async (prisma) => {
-        // Code running in a transaction...
-    },
-    {
-        maxWait: 5000, // default: 2000
-        timeout: 10000, // default: 5000
-    }
-)
-    ;
+export const prismaClient = new PrismaClient();
